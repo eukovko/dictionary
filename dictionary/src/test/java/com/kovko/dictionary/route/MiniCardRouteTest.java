479798faeaca165b83a21570a1a8c08a9dd8ffa8
eu.kovko <eu.kovko@gmail.com>
2020-08-20 12:17:03 +0300

    Fixed a minicard test

--- dictionary/src/test/java/com/kovko/dictionary/route/MiniCardRouteTest.java
+++ dictionary/src/test/java/com/kovko/dictionary/route/MiniCardRouteTest.java
@@ -2,19 +2,20 @@ package com.kovko.dictionary.route;
 
 import com.kovko.dictionary.aggregator.AuthenticationAggregator;
 import com.kovko.dictionary.config.CamelConfig;
-import com.kovko.dictionary.processor.AuthenticationProcessor;
-import com.kovko.dictionary.processor.HttpOperationFailedProcessor;
+import com.kovko.dictionary.mapper.Mapper;
+import com.kovko.dictionary.processor.*;
+import com.kovko.dictionary.repository.MinicardRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.spi.DataFormat;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -30,7 +31,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 @CamelSpringBootTest
 @EnableAutoConfiguration
-@ContextConfiguration(classes = {MiniCardRoute.class, HttpOperationFailedProcessor.class, AuthenticationRoute.class, AuthenticationAggregator.class, AuthenticationProcessor.class}, initializers = ConfigFileApplicationContextInitializer.class)
+@ContextConfiguration(classes = {MiniCardRoute.class,
+        HttpOperationFailedProcessor.class, AuthenticationRoute.class,
+        AuthenticationAggregator.class, AuthenticationProcessor.class,
+        BatchRestProcessor.class, MinicardTranslationProcessor.class,
+        Mapper.class, MiniCardDatabaseProcessor.class},
+        initializers = ConfigFileApplicationContextInitializer.class)
+@MockBean(classes = MinicardRepository.class)
 @Import(CamelConfig.class)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -39,17 +46,14 @@ class MiniCardRouteTest {
     @EndpointInject("mock:output")
     MockEndpoint mockEndpoint;
 
-    @Autowired
-    DataFormat dataFormat;
-
     @Test
     void testMiniCardRestEndpoint(@Autowired CamelContext context) throws Exception {
 
-        AdviceWithRouteBuilder.adviceWith(context, "dictionary", s -> s.weaveByToUri("{{api.translation.minicard.input}}").replace().to("mock:output"));
+        AdviceWithRouteBuilder.adviceWith(context, "api.translation.minicard.input", s -> s.weaveByToUri("direct:minicard").replace().to("mock:output"));
 
         RestTemplate restTemplate = new RestTemplate();
 
-        int statusCode = restTemplate.getForEntity("http://localhost:9090/hello?text=hello&srcLang=1033&dstLang=1049", String.class)
+        int statusCode = restTemplate.getForEntity("http://localhost:9090/minicard?text=hello&srcLang=1033&dstLang=1049", String.class)
                 .getStatusCodeValue();
 
         assertThat(statusCode).isEqualTo(200);
